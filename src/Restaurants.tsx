59d15c9b61464d16b162ba61eebd9975c91906df
Julio Mojica <juliocmojica@gmail.com>
2022-11-19 06:29:44 -0500

    Struct restaurant page

--- src/Restaurants.tsx
+++ src/Restaurants.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export function Restaurants() {
+
+    const [foodTypes, setFoodType] = useState([])
+
+    useEffect(() => {
+        (
+            async function() {
+                console.log('fetching food types and restaurants')
+            }
+        )()
+    }, [])
+
     return (
-        <div>
-            Hello
+        <div style={{display: "flex", flexDirection: 'row', minWidth: '100vw', minHeight: "100vh"}}>
+            <div style={{width: '25%', background: 'blue'}}></div>
+            <div style={{width: '75%', background: 'red'}}></div>
         </div>
     )
 }
